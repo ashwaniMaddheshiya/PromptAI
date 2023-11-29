ca46f8f2b2b435bce8e73cd459b874f147e076a4
ashwaniMaddheshiya <98683284+ashwaniMaddheshiya@users.noreply.github.com>
2023-11-29 16:32:12 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "https://promptai-client.onrender.com/prompt",
+    origin: "https://promptai-client.onrender.com",
   })
 );
 
