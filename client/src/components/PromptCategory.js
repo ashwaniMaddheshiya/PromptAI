69669466627d64321384cc97d1efe0d48b1b1c26
ashwaniMaddheshiya <maddheshiya.aashu@gmail.com>
2024-01-02 10:41:19 +0530

    Performance optimized

--- client/src/components/PromptCategory.js
+++ client/src/components/PromptCategory.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import bgImg from "../card-bg.jpg";
+import { FaChevronRight } from "react-icons/fa";
 import { toast } from "react-toastify";
 
 const PromptCategory = () => {
@@ -10,12 +10,10 @@ const PromptCategory = () => {
   useEffect(() => {
     const fetchCategory = async () => {
       try {
-        const response = await axios.get(
-          "/api/category/all"
-        );
+        const response = await axios.get("/api/category/all");
         setCategory(response.data.category);
       } catch (err) {
-        console.log(err)
+        console.log(err);
         toast.error("Something went wrong, Please try again");
       }
     };
@@ -27,15 +25,14 @@ const PromptCategory = () => {
     <div className="flex items-center justify-around flex-wrap">
       {category.map((prompt) => (
         <Link to={`/prompt/${prompt._id}`}>
-          <div
-            className="w-80 my-4 mx-4 rounded overflow-hidden shadow-lg px-6 py-8"
-            style={{
-              backgroundImage: `url(${bgImg})`,
-              backgroundSize: "cover",
-            }}
-          >
-            <div className="text-2xl font-medium">{prompt.category}</div>
-            <div>Prompts</div>
+          <div className="w-80 my-4 mx-4 rounded overflow-hidden shadow-lg px-6 py-8 border flex justify-between items-center">
+            <div>
+              <div className="text-2xl font-medium">{prompt.category}</div>
+              <div>Prompts</div>
+            </div>
+            <div>
+              <FaChevronRight size={20} />
+            </div>
           </div>
         </Link>
       ))}
